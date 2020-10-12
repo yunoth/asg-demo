bbff4564e8c961276b9d731c69cb61acdcdf1ced
Yuno <yunoth51@gmail.com>
2020-10-12 09:26:53 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,8 +2,7 @@ variable "region" {}
 variable "az" {}
 variable "alb_name" {}
 variable "db-username" {}
-variable "db-password" {}
-
+variable "db-password" { default = "changeme"}
 
 variable "cfn_stack_name" {}
 variable "cfn_signal_count" {}
